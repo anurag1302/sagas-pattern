e520f572159163e1dada8842e49ecc50a93c63e1
Anurag <anurag3487@gmail.com>
2026-01-27 23:09:09 +0530

    inventory method created

---
+++ orchestration/Sagas.Orchestration/InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,33 @@
+﻿using InventoryService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        public Guid HardcodedId { get; set; }
+        public InventoryController() 
+        {
+            HardcodedId = Guid.NewGuid();
+        }
+
+        public IActionResult ReserveInventory([FromBody] Order order)
+        {
+            if(order == null)
+            {
+                return BadRequest();
+            }
+            if(order.Id != HardcodedId)
+            {
+                return BadRequest("Order out of stock");
+            }
+
+            Console.WriteLine("Inventory Reserved");
+            return Ok();
+
+        }
+    }
+}
---
+++ orchestration/Sagas.Orchestration/InventoryService/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿namespace InventoryService.Models
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public decimal? Price { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+
+    public enum OrderStatus
+    {
+        OrderInitiated,
+        PaymentProcessed,
+        InventoryReserved,
+        OrderCompleted,
+        OrderCancelled
+    }
+}
