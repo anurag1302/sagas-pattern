bc4adf832a3aea9fd200ca6dd66114452c96bf85
Anurag <anurag3487@gmail.com>
2026-01-27 22:13:21 +0530

    DI for Order Service

--- orchestration/Sagas.Orchestration/OrderService/Program.cs
+++ orchestration/Sagas.Orchestration/OrderService/Program.cs
@@ -1,4 +1,6 @@
 
+using OrderService.Services;
+
 namespace OrderService
 {
     public class Program
@@ -13,6 +15,7 @@ namespace OrderService
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<OrderRepository>();
 
             var app = builder.Build();
 
